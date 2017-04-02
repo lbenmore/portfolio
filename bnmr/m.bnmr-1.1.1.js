188bb531311dffcf0e17515a9f7e5640bfbce9bf
Lisa <lbenmore@gmail.com>
2017-04-02 09:36:09 -0700

    added dev folder, favicon, gitignore, and updated beautification of bnmr/m.bnmr-1.1.1.js

--- bnmr/m.bnmr-1.1.1.js
+++ bnmr/m.bnmr-1.1.1.js
@@ -1,325 +1,326 @@
 // JavaScript Document
+var $$ = function(sel) {
+    var el = sel || 'body',
+        _this = [],
+        i,
 
-var $$ = function (sel) {
-	var el = sel || 'body',
-		_this = [],
-		i,
+        addCSS,
+        ajax,
+        contains,
+        getParam,
+        isMobile,
+        load,
+        preload;
 
-		addCSS,
-		ajax,
-		contains,
-		getParam,
-		isMobile,
-		load,
-		preload;
+    el.slice(0, 1) == '#' ? _this = document.querySelector(el) : _this = document.querySelectorAll(el);
+    (typeof _this == 'object' && _this.length == 1) ? _this = _this[0]: null;
 
-  el.slice(0, 1) == '#' ? _this = document.querySelector(el) : _this = document.querySelectorAll(el);
-	(typeof _this == 'object' && _this.length == 1) ? _this = _this[0] : null;
-
-	function setProp(prop, val) {
-		if (_this.length) {
-			for (i = 0; i < _this.length; i++) {
-				_this[i].style[prop] = val;
-			}
-		} else {
-			_this.style[prop] = val;
-		}
-	}
-
-	function preloadAsset(asset) {
-		var ext = asset.split('.')[asset.split('.').length - 1].toLowerCase();
-
-		switch (ext) {
-			case 'jpg':
-			case 'jpeg':
-			case 'gif':
-			case 'png':
-				new Image().src = asset;
-			break;
-
-			case 'mp4':
-			case 'ogv':
-			case 'webm':
-				var newVid = document.createElement('video');
-				newVid.src = asset;
-				newVid.load();
-			break;
-		}
-	}
-
-	function loadAsset(asset) {
-		var ext = asset.split('.')[asset.split('.').length - 1].toLowerCase();
-
-		switch (ext) {
-			case 'css':
-				var link = document.createElement('link');
-				link.rel = 'stylesheet';
-				link.href = asset;
-
-				var head = document.getElementsByTagName('head')[0];
-				head.appendChild(link);
-			break;
-
-			case 'js':
-				var script = document.createElement('script');
-				script.type = 'text/javascript';
-				script.async = 'true';
-				script.src = asset;
-
-				document.body.appendChild(script);
-			break;
-		}
-	}
-
-	function evalTouch (dir, fn) {
-		var startTouch, endTouch;
-
-		_this.listener('touchstart', function (e) {
-			e.preventDefault();
-			startTouch = {
-				'x' : e.touches[0].clientX,
-				'y' : e.touches[0].clientY
-			};
-		});
-
-		_this.listener('touchend', function (e) {
-			e.preventDefault();
-			endTouch = {
-				'x' : e.changedTouches[0].clientX,
-				'y' : e.changedTouches[0].clientY
-			};
-
-			switch (dir) {
-				case 'tap':
-					if (Math.abs(startTouch.x - endTouch.x) < 100 && Math.abs(startTouch.y - endTouch.y) < 100) {
-						fn();
-					}
-				break;
-
-				case 'left':
-					if (Math.abs(startTouch.x - endTouch.x) > 100 && startTouch.x > endTouch.x) {
-						fn();
-					}
-				break;
-
-				case 'right':
-					if (Math.abs(startTouch.x - endTouch.x) > 100 &&startTouch.x < endTouch.x) {
-						fn();
-					}
-				break;
-
-				case 'up':
-					if (Math.abs(startTouch.y - endTouch.y) > 100 && startTouch.y > endTouch.y) {
-						fn();
-					}
-				break;
-
-				case 'down':
-					if (Math.abs(startTouch.y - endTouch.y) > 100 &&startTouch.y < endTouch.y) {
-						fn();
-					}
-				break;
-			}
-		});
-
-	};
-
-  _this.on = function (evt, fn) {
-    switch (evt) {
-      case 'swipeleft':
-				evalTouch('left', fn);
-      break;
-
-      case 'swiperight':
-				evalTouch('right', fn);
-      break;
-
-      case 'swipeup':
-				evalTouch('up', fn);
-      break;
-
-      case 'swipedown':
-				evalTouch('down', fn);
-      break;
-
-      case 'tap':
-				evalTouch('tap', fn);
-      break;
-
-			case 'click':
-			case 'mouseover':
-			case 'mouseout':
-			break;
+    function setProp(prop, val) {
+        if (_this.length) {
+            for (i = 0; i < _this.length; i++) {
+                _this[i].style[prop] = val;
+            }
+        } else {
+            _this.style[prop] = val;
+        }
     }
-  };
 
-	_this.css = function (prop, val, del){
-		var delay = del || 0;
-		setTimeout(setProp, delay, prop, val);
-	};
+    function preloadAsset(asset) {
+        var ext = asset.split('.')[asset.split('.').length - 1].toLowerCase();
 
-	_this.animate = function (props, dur, del, eas){
-		var transitionStyle = '';
-		var options = {};
-		var counter = 0;
-		options.properties = props;
-		options.duration = dur || '500';
-		options.delay = del || '0';
-		options.ease = eas || 'ease';
+        switch (ext) {
+            case 'jpg':
+            case 'jpeg':
+            case 'gif':
+            case 'png':
+                new Image().src = asset;
+                break;
 
-		for (var i in options.properties) {
-			counter++;
-		}
+            case 'mp4':
+            case 'ogv':
+            case 'webm':
+                var newVid = document.createElement('video');
+                newVid.src = asset;
+                newVid.load();
+                break;
+        }
+    }
 
-		for (var key in options.properties) {
-			var property = key;
-			var value = options.properties[key];
-			counter--;
+    function loadAsset(asset) {
+        var ext = asset.split('.')[asset.split('.').length - 1].toLowerCase();
 
-			if (counter > 0) {
-				transitionStyle += property + ' ' + options.duration + 'ms ' + options.ease + ', ';
-			} else {
-				transitionStyle += property + ' ' + options.duration + 'ms ' + options.ease;
-			}
+        switch (ext) {
+            case 'css':
+                var link = document.createElement('link');
+                link.rel = 'stylesheet';
+                link.href = asset;
 
-			_this.css(property, value, options.delay);
-		}
+                var head = document.getElementsByTagName('head')[0];
+                head.appendChild(link);
+                break;
 
-		if (_this.length) {
-			for (i = 0; i < _this.length; i++) {
-				if (_this[i].style.transition !== "" || _this[i].style.webkitTransform !== "") {
-					transitionStyle = ', ' + transitionStyle;
-				}
-				_this[i].style.webkitTransition += transitionStyle;
-				_this[i].style.transition += transitionStyle;
-			}
-		} else {
-			if (_this.style.transition !== "" || _this.style.webkitTransform !== "") {
-				transitionStyle = ', ' + transitionStyle;
-			}
-			_this.style.webkitTransition += transitionStyle;
-			_this.style.transition += transitionStyle;
-		}
-	};
+            case 'js':
+                var script = document.createElement('script');
+                script.type = 'text/javascript';
+                script.async = 'true';
+                script.src = asset;
 
-	_this.listener = function (evt, fn) {
-		for (i = 0; i < _this.length; i++) {
-			if (window.attachEvent) {
-				_this[i].attachEvent('on' + evt, fn);
-			} else if (window.addEventListener) {
-				_this[i].addEventListener(evt, fn);
-			} else {
-				console.error('No Attach Event Found');
-			}
-		}
-	};
+                document.body.appendChild(script);
+                break;
+        }
+    }
 
-	ajax = function (options, callback, file) {
-		var xhr = new XMLHttpRequest(),
-				type = options.type || 'ajax',
-				method = options.method || 'GET',
-				url = options.url || '',
-				isAsync = options.async || true;
+    function evalTouch(dir, fn) {
+        var startTouch, endTouch;
 
-		xhr.open(method, url, isAsync);
-		if (file) {
-			xhr.setRequestHeader('X_FILENAME', file.name);
-			xhr.send(file);
-		} else {
-			xhr.send();
-		}
+        _this.listener('touchstart', function(e) {
+            e.preventDefault();
+            startTouch = {
+                'x': e.touches[0].clientX,
+                'y': e.touches[0].clientY
+            };
+        });
 
-    xhr.onreadystatechange = function () {
-      if (xhr.readyState == 4) {
-		    switch (type) {
-		    	case 'json':
-			        if (callback) {
-			          callback(JSON.parse(xhr.responseText));
-			        }
-			        return JSON.parse(xhr.responseText);
-		    	break;
+        _this.listener('touchend', function(e) {
+            e.preventDefault();
+            endTouch = {
+                'x': e.changedTouches[0].clientX,
+                'y': e.changedTouches[0].clientY
+            };
 
-		    	default:
-			        if (callback) {
-			          callback(xhr.responseText);
-			        }
-			        return xhr.responseText;
-		    	break;
-		    }
-      }
+            switch (dir) {
+                case 'tap':
+                    if (Math.abs(startTouch.x - endTouch.x) < 100 && Math.abs(startTouch.y - endTouch.y) < 100) {
+                        fn();
+                    }
+                    break;
+
+                case 'left':
+                    if (Math.abs(startTouch.x - endTouch.x) > 100 && startTouch.x > endTouch.x) {
+                        fn();
+                    }
+                    break;
+
+                case 'right':
+                    if (Math.abs(startTouch.x - endTouch.x) > 100 && startTouch.x < endTouch.x) {
+                        fn();
+                    }
+                    break;
+
+                case 'up':
+                    if (Math.abs(startTouch.y - endTouch.y) > 100 && startTouch.y > endTouch.y) {
+                        fn();
+                    }
+                    break;
+
+                case 'down':
+                    if (Math.abs(startTouch.y - endTouch.y) > 100 && startTouch.y < endTouch.y) {
+                        fn();
+                    }
+                    break;
+            }
+        });
+
     };
-	};
 
-	preload = function (assets) {
-		if (assets instanceof Array) {
-			for (i = 0; i < assets.length; i++) {
-				preloadAsset(assets[i]);
-			}
-		} else {
-			preloadAsset(assets);
-		}
-	};
+    _this.on = function(evt, fn) {
+        switch (evt) {
+            case 'swipeleft':
+                evalTouch('left', fn);
+                break;
 
-	load = function (assets, callback) {
-		if (assets instanceof Array) {
-			for (i = 0; i < assets.length; i++) {
-				loadAsset(assets[i]);
-			}
-		} else {
-			loadAsset(assets);
-		}
+            case 'swiperight':
+                evalTouch('right', fn);
+                break;
 
-		if (callback) {
-			callback();
-		}
-	};
+            case 'swipeup':
+                evalTouch('up', fn);
+                break;
 
-	contains = function (string, substring) {
-		return string.indexOf(substring) > -1;
-	};
+            case 'swipedown':
+                evalTouch('down', fn);
+                break;
 
-	addCSS = function (el, prop, val) {
-		var ss = document.styleSheets[document.styleSheets.length - 1];
+            case 'tap':
+                evalTouch('tap', fn);
+                break;
 
-		if (ss) {
-			var sslen = ss.cssRules ? ss.cssRules.length : ss.rules.length;
-			ss.insertRule(el + ' {' + prop + ': ' + val + '; }', sslen);
-		} else if (document.getElementsByTagName('style').length) {
-			var styleTag = document.getElementsByTagName('style')[document.getElementsByTagName('style').length - 1];
-			styleTag.innerHTML += ' \r' + el + '{' + prop + ':' + val + '}';
-		} else {
-			var styleTag = document.createElement('style');
-			styleTag.innerHTML = el + '{' + prop + ':' + val + '}';
-		}
-	};
+            case 'click':
+            case 'mouseover':
+            case 'mouseout':
+                break;
+        }
+    };
 
-	getParam = function (key) {
-		var params = String(window.location).split('?')[1];
+    _this.css = function(prop, val, del) {
+        var delay = del || 0;
+        setTimeout(setProp, delay, prop, val);
+    };
 
-		if (params && params !== null && params !== undefined && params !== '') {
-			var pairs = params.split('&');
+    _this.animate = function(props, dur, del, eas) {
+        var transitionStyle = '';
+        var options = {};
+        var counter = 0;
+        options.properties = props;
+        options.duration = dur || '500';
+        options.delay = del || '0';
+        options.ease = eas || 'ease';
 
-			for (var i = 0; i < pairs.length; i++) {
-				if (pairs[i].split('=')[0] == key) {
-					return pairs[i].split('=')[1];
-				}
-			}
-		}
-	};
+        for (var i in options.properties) {
+            counter++;
+        }
 
-  isMobile = function () {
-    return (/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i).test(navigator.userAgent);
-  };
+        for (var key in options.properties) {
+            var property = key;
+            var value = options.properties[key];
+            counter--;
 
-	$$.addCSS = addCSS;
-	$$.ajax = ajax;
-	$$.contains = contains;
-	$$.getParam = getParam;
-  $$.isMobile = isMobile;
-	$$.load = load;
-	$$.preload = preload;
+            if (counter > 0) {
+                transitionStyle += property + ' ' + options.duration + 'ms ' + options.ease + ', ';
+            } else {
+                transitionStyle += property + ' ' + options.duration + 'ms ' + options.ease;
+            }
 
-	return _this;
+            _this.css(property, value, options.delay);
+        }
+
+        if (_this.length) {
+            for (i = 0; i < _this.length; i++) {
+                if (_this[i].style.transition !== "" || _this[i].style.webkitTransform !== "") {
+                    transitionStyle = ', ' + transitionStyle;
+                }
+                _this[i].style.webkitTransition += transitionStyle;
+                _this[i].style.transition += transitionStyle;
+            }
+        } else {
+            if (_this.style.transition !== "" || _this.style.webkitTransform !== "") {
+                transitionStyle = ', ' + transitionStyle;
+            }
+            _this.style.webkitTransition += transitionStyle;
+            _this.style.transition += transitionStyle;
+        }
+    };
+
+    _this.listener = function(evt, fn) {
+        for (i = 0; i < _this.length; i++) {
+            if (window.attachEvent) {
+                _this[i].attachEvent('on' + evt, fn);
+            } else if (window.addEventListener) {
+                _this[i].addEventListener(evt, fn);
+            } else {
+                console.error('No Attach Event Found');
+            }
+        }
+    };
+
+    ajax = function(options, callback, file) {
+        var xhr = new XMLHttpRequest(),
+            type = options.type || 'ajax',
+            method = options.method || 'GET',
+            url = options.url || '',
+            isAsync = options.async || true;
+
+        xhr.open(method, url, isAsync);
+        if (file) {
+            xhr.setRequestHeader('X_FILENAME', file.name);
+            xhr.send(file);
+        } else {
+            xhr.send();
+        }
+
+        xhr.onreadystatechange = function() {
+            if (xhr.readyState == 4) {
+                switch (type) {
+                    case 'json':
+                        if (callback) {
+                            callback(JSON.parse(xhr.responseText));
+                        }
+                        return JSON.parse(xhr.responseText);
+                        break;
+
+                    default:
+                        if (callback) {
+                            callback(xhr.responseText);
+                        }
+                        return xhr.responseText;
+                        break;
+                }
+            }
+        };
+    };
+
+    preload = function(assets) {
+        if (assets instanceof Array) {
+            for (i = 0; i < assets.length; i++) {
+                preloadAsset(assets[i]);
+            }
+        } else {
+            preloadAsset(assets);
+        }
+    };
+
+    load = function(assets, callback) {
+        if (assets instanceof Array) {
+            for (i = 0; i < assets.length; i++) {
+                loadAsset(assets[i]);
+            }
+        } else {
+            loadAsset(assets);
+        }
+
+        if (callback) {
+            callback();
+        }
+    };
+
+    contains = function(string, substring) {
+        return string.indexOf(substring) > -1;
+    };
+
+    addCSS = function(el, prop, val) {
+        var ss = document.styleSheets[document.styleSheets.length - 1];
+
+        if (ss) {
+            var sslen = ss.cssRules ? ss.cssRules.length : ss.rules.length;
+            ss.insertRule(el + ' {' + prop + ': ' + val + '; }', sslen);
+        } else if (document.getElementsByTagName('style').length) {
+            var styleTag = document.getElementsByTagName('style')[document.getElementsByTagName('style').length - 1];
+            styleTag.innerHTML += ' \r' + el + '{' + prop + ':' + val + '}';
+        } else {
+            var styleTag = document.createElement('style');
+            styleTag.innerHTML = el + '{' + prop + ':' + val + '}';
+        }
+    };
+
+    getParam = function(key) {
+        var params = String(window.location).split('?')[1];
+
+        if (params && params !== null && params !== undefined && params !== '') {
+            var pairs = params.split('&');
+
+            for (var i = 0; i < pairs.length; i++) {
+                if (pairs[i].split('=')[0] == key) {
+                    return pairs[i].split('=')[1];
+                }
+            }
+        }
+    };
+
+    isMobile = function() {
+        return (/Android|BlackBerry|iPhone|iPad|iPod|Opera Mini|IEMobile|WPDesktop/i).test(navigator.userAgent);
+    };
+
+    $$.addCSS = addCSS;
+    $$.ajax = ajax;
+    $$.contains = contains;
+    $$.getParam = getParam;
+    $$.isMobile = isMobile;
+    $$.load = load;
+    $$.preload = preload;
+
+    return _this;
 };
 
-(function (){ $$(); }());
+(function() {
+    $$();
+}());
---
+++ dev/scripts/library.js
@@ -0,0 +1,98 @@
+// JavaScript Document
+
+let bnmr = {};
+bnmr.vars = {};
+bnmr.fns = {};
+
+bnmr.vars.project = 'Portfolio';
+bnmr.vars.version = '1.0.0';
+bnmr.vars.debugg = true;
+
+bnmr.fns.log = (msg, caller, type) => {
+  type = type || 'log';
+  caller = caller || 'unknown';
+
+  if (bnmr.vars.debugg) {
+    if (typeof msg == 'string' || !window.console.dir) {
+      console[type](bnmr.vars.project + ' :: ' + bnmr.vars.version + ' :: ' + caller + ' :: ' + msg);
+    } else {
+      console[type](bnmr.vars.project + ' :: ' + bnmr.vars.version + ' :: ' + caller);
+      console.dir(msg);
+    }
+  }
+};
+
+bnmr.fns.initCards = () => {
+  $$.ajax({
+    'type': 'json',
+    'url': './scripts/projects.json'
+  }, (projects) => {
+    bnmr.fns.log('Projects retrieved', 'initCards');
+    for (let project of projects) {
+      bnmr.fns.log(project, 'initCards Project:');
+      let card = document.createElement('div');
+      card.className = 'projects__card';
+      $$('.content__projects').appendChild(card);
+
+        let header = document.createElement('header');
+        header.className = 'card__header';
+        card.appendChild(header);
+
+          let title = document.createElement('h1');
+          title.className = 'card__title';
+          title.innerHTML = project.name;
+          header.appendChild(title);
+
+          let technologies = document.createElement('h2');
+          technologies.className = 'card__subtitle';
+          technologies.innerHTML = '<em>Technologies: ' + project.technologies + '</em>';
+          header.appendChild(technologies);
+
+          let browsers = document.createElement('h2');
+          browsers.className = 'card__subtitle';
+          browsers.innerHTML = '<em>Browsers: ' + project.browsers + '</em>';
+          header.appendChild(browsers);
+
+        let description = document.createElement('section');
+        description.className = 'card__description';
+        description.innerHTML = project.description;
+        card.appendChild(description);
+
+        let links = document.createElement('section');
+        links.className = 'card__links';
+        card.appendChild(links);
+
+          for (let link of project.links) {
+            let button = document.createElement('button');
+            button.className = 'links__button';
+            button.innerHTML = link.name;
+            button.setAttribute('data-link', link.url);
+            links.appendChild(button);
+
+            button.onclick = (e) => {
+              window.open(e.target.getAttribute('data-link'), '_blank');
+            };
+          }
+    }
+  });
+};
+
+
+bnmr.fns.eventListeners = () => {
+
+};
+
+bnmr.fns.initFns = () => {
+  bnmr.fns.log('Begin initialization', 'initFns');
+
+  bnmr.fns.eventListeners();
+  bnmr.fns.initCards();
+};
+
+const libraryLoadInterval = setInterval(() => {
+  bnmr.fns.log('Loading...', 'libraryLoadInterval');
+  if ($$) {
+    clearInterval(libraryLoadInterval);
+    bnmr.fns.initFns();
+  }
+}, 100);
