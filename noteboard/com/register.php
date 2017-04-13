ad0b092efe31f393a7adb049e59206614fdc6e8b
Lisa <lbenmore@gmail.com>
2017-04-13 09:32:15 -0700

    Updated login to check for username in database

--- noteboard/com/register.php
+++ noteboard/com/register.php
@@ -6,15 +6,24 @@
 
   if (isset($fullname) && !empty($fullname) && isset($username) && !empty($username) && isset($password) && !empty($password) && isset($confirm) && !empty($confirm) && $password == $confirm) {
     $conn = connect_to_noteboard();
-    $query_create_user = "INSERT INTO users (fullname, username, password) VALUES (\"$fullname\", \"$username\", \"$password\");";
-    $create_user = $conn->query($query_create_user);
+    
+    $check_for_user_query = "SELECT * FROM users WHERE username='$username';";
+    $check_for_user = $conn->query($check_for_user_query)->fetch_assoc();
+    
+    if (!$check_for_user) {
+      $query_create_user = "INSERT INTO users (fullname, username, password) VALUES (\"$fullname\", \"$username\", \"$password\");";
+      $create_user = $conn->query($query_create_user);
 
-    include("login.php");
+      include("login.php");
+    } else {
+      echo "Username already exists. Please try again. \n";
+      include("./com/form_register.php");
+    }
   } elseif ($password != $confirm) {
-    echo "Password fields did not match.";
+    echo "Password fields did not match. \n";
     include("./com/form_register.php");
   } else {
-    echo "Please fill in all fields.";
+    echo "Please fill in all fields. \n";
     include("./com/form_register.php");
   }
 ?>
