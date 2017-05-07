5af0afcce5a4b87f143d9e121d94d6060fe80919
Lisa <lbenmore@gmail.com>
2017-05-07 09:08:12 -0700

    Updates for mobile functionality

--- startle-revisited/library.js
+++ startle-revisited/library.js
@@ -8,7 +8,7 @@ bnmr.$ = function (selector) {
 	var _this;
 	_this = selector.slice(0, 1) == '#' ? document.querySelector(selector) : document.querySelectorAll(selector);
 	(typeof _this == 'object' && _this.length == 1) ? _this = _this[0] : null;
-	
+
 	_this.listener = function (event, callback) {
 		if (window.addEventListener) {
 			_this.addEventListener(event, callback);
@@ -16,7 +16,7 @@ bnmr.$ = function (selector) {
 			_this.attachEvent('on' + event, callback);
 		}
 	};
-	
+
 	_this.execTouch = function (e, event, callback) {
 		switch (event) {
 			case 'swipeleft':
@@ -24,37 +24,39 @@ bnmr.$ = function (selector) {
 					callback(e);
 				}
 			break;
-				
+
 			case 'swiperight':
 				if (Math.abs(bnmr.vars.startX - bnmr.vars.endX) > 100 && bnmr.vars.startX < bnmr.vars.endX) {
 					callback(e);
 				}
 			break;
-				
+
 			case 'swipeup':
 				if (Math.abs(bnmr.vars.startY - bnmr.vars.endY) > 100 && bnmr.vars.startY > bnmr.vars.endY) {
 					callback(e);
 				}
 			break;
-				
+
 			case 'swipedown':
 				if (Math.abs(bnmr.vars.startY - bnmr.vars.endY) > 100 && bnmr.vars.startY < bnmr.vars.endY) {
 					callback(e);
 				}
 			break;
-				
-			case 'tap':	
+
+			case 'tap':
 				if (Math.abs(bnmr.vars.startX - bnmr.vars.endX) < 50 && Math.abs(bnmr.vars.startY - bnmr.vars.endY) < 50) {
 					callback(e);
 				}
 			break;
-			
+
 			default:
 			break;
 		}
 	};
-	
+
 	_this.evalTouch = function (event, callback) {
+		var hasMouseUp = false;
+
 		_this.on('mousedown', function (e) {
 			bnmr.vars.startX = e.clientX;
 			bnmr.vars.startY = e.clientY;
@@ -63,19 +65,23 @@ bnmr.$ = function (selector) {
 			bnmr.vars.startX = e.changedTouches[0].clientX;
 			bnmr.vars.startY = e.changedTouches[0].clientY;
 		});
-		
+
 		_this.on('mouseup', function (e) {
+			hasMouseUp = true;
+			
 			bnmr.vars.endX = e.clientX;
 			bnmr.vars.endY = e.clientY;
 			_this.execTouch(e, event, callback);
 		});
 		_this.on('touchend', function (e) {
-			bnmr.vars.endX = e.changedTouches[0].clientX;
-			bnmr.vars.endY = e.changedTouches[0].clientY;
-			_this.execTouch(e, event, callback);
+			if (!hasMouseUp) {
+				bnmr.vars.endX = e.changedTouches[0].clientX;
+				bnmr.vars.endY = e.changedTouches[0].clientY;
+				_this.execTouch(e, event, callback);
+			}
 		});
 	}
-	
+
 	_this.on = function (event, callback) {
 		switch (event) {
 			case 'swipeleft':
@@ -85,13 +91,13 @@ bnmr.$ = function (selector) {
 			case 'tap':
 				_this.evalTouch(event, callback);
 			break;
-				
+
 			default:
 				_this.listener(event, callback);
 			break;
 		}
 	};
-	
+
 	_this.addClass = function (cName, delay) {
 		setTimeout(function () {
 			if (document.body.classList) {
@@ -101,7 +107,7 @@ bnmr.$ = function (selector) {
 			}
 		}, delay || 0);
 	};
-	
+
 	_this.removeClass = function (cName, delay) {
 		setTimeout(function () {
 			if (document.body.classList) {
@@ -111,13 +117,13 @@ bnmr.$ = function (selector) {
 			}
 		}, delay || 0);
 	};
-	
+
 	_this.replaceClass = function (cName, cName2, delay) {
 		setTimeout(function () {
 			_this.className = _this.className.replace(cName, cName2);
 		}, delay || 0);
 	};
-	
+
 	_this.toggleClass = function (cName, delay) {
 		if (_this.className.indexOf(cName) > -1) {
 			_this.removeClass(cName, delay)
@@ -125,7 +131,7 @@ bnmr.$ = function (selector) {
 			 _this.addClass(cName, delay);
 		 }
 	};
-	
+
 	return _this;
 };
 
@@ -137,7 +143,7 @@ bnmr.fns.initClockFace = function () {
 				second = date.getSeconds(),
 				strHour,
 				strMinute;
-	
+
 		if (hour == 0) {
 			strHour = String(12);
 		} else if (hour > 12) {
@@ -145,22 +151,22 @@ bnmr.fns.initClockFace = function () {
 		} else {
 			strHour = String(hour);
 		}
-		
+
 		if (String(minute).length == 1) {
 			strMinute = '0' + minute;
 		} else {
 			strMinute = String(minute);
 		}
-	
+
 		// Digital Face
 		bnmr.$('.display__digit--hour').innerHTML = strHour;
 		bnmr.$('.display__digit--minute').innerHTML = strMinute;
-	
+
 		// Analog Face
 		bnmr.$('.clock__hand--second').style.webkitTransform = 'rotate(' + second * 6 + 'deg) translateY(-100%)';
 		bnmr.$('.clock__hand--minute').style.webkitTransform = 'rotate(' + minute * 6 + 'deg) translateY(-100%)';
 		bnmr.$('.clock__hand--hour').style.webkitTransform = 'rotate(' + hour * 30 + 'deg) translateY(-100%)';
-		
+
 		bnmr.$('.clock__hand--second').style.transform = 'rotate(' + second * 6 + 'deg) translateY(-100%)';
 		bnmr.$('.clock__hand--minute').style.transform = 'rotate(' + minute * 6 + 'deg) translateY(-100%)';
 		bnmr.$('.clock__hand--hour').style.transform = 'rotate(' + hour * 30 + 'deg) translateY(-100%)';
@@ -172,18 +178,18 @@ bnmr.fns.togglePanel = function () {
 		case 'clock':
 			bnmr.$('.clock').toggleClass('hidden');
 			bnmr.$('.config').toggleClass('hidden');
-			
+
 			bnmr.vars.panelState = 'config';
 		break;
-			
+
 		case 'config':
 			bnmr.$('.clock').toggleClass('hidden');
 			bnmr.$('.config').toggleClass('hidden');
-			
-			
+
+
 			bnmr.vars.panelState = 'clock';
 		break;
-			
+
 		default:
 			console.error('Something has gone awry!');
 		break;
@@ -194,7 +200,7 @@ bnmr.fns.closeResetConfig = function () {
 	bnmr.$('.options__dropdown--time').value = '';
 	bnmr.$('.options__dropdown--sound').value = '';
 	bnmr.$('.options__dropdown--gestures').value = '';
-	
+
 	bnmr.$('.config__options').addClass('hidden');
 };
 
@@ -203,7 +209,7 @@ bnmr.fns.optionsCancel = function () {
 		case 'add':
 			bnmr.fns.closeResetConfig();
 		break;
-			
+
 		case 'edit':
 			bnmr.fns.alarms('remove');
 		break;
@@ -226,7 +232,7 @@ bnmr.fns.optionsSubmit = function () {
 				}
 			}
 		break;
-			
+
 		case 'edit':
 			bnmr.fns.alarms('update');
 		break;
@@ -236,23 +242,23 @@ bnmr.fns.optionsSubmit = function () {
 bnmr.fns.editAlarm = function (liAlarm) {
 	bnmr.vars.configState = 'edit';
 	bnmr.vars.selectedAlarm = liAlarm.getAttribute('data-alarm-id');
-	
+
 	bnmr.$('.options__btn--submit').innerHTML = 'Update';
 	bnmr.$('.options__btn--cancel').innerHTML = 'Delete';
-	
+
 	bnmr.$('.options__dropdown--time').value = liAlarm.getAttribute('data-alarm-time');
 	bnmr.$('.options__dropdown--sound').value = liAlarm.getAttribute('data-alarm-sound');
 	bnmr.$('.options__dropdown--gestures').value = liAlarm.getAttribute('data-alarm-gestures');
-	
+
 	bnmr.$('.config__options').removeClass('hidden');
 };
 
 bnmr.fns.addAlarm = function () {
 	bnmr.vars.configState = 'add';
-	
+
 	bnmr.$('.options__btn--submit').innerHTML = 'Save';
 	bnmr.$('.options__btn--cancel').innerHTML = 'Cancel';
-	
+
 	bnmr.$('.config__options').removeClass('hidden');
 }
 
@@ -266,11 +272,11 @@ bnmr.fns.buildAlarmList = function () {
 		liAlarm.setAttribute('data-alarm-sound', bnmr.vars.alarms[i].sound);
 		liAlarm.setAttribute('data-alarm-gestures', bnmr.vars.alarms[i].gestures);
 		liAlarm.setAttribute('data-alarm-id', bnmr.vars.alarms[i].id);
-		
+
 		liAlarm.addEventListener('mouseup', function (e) {
 			bnmr.fns.editAlarm(e.target);
 		});
-		
+
 		bnmr.$('.list__ul').insertBefore(liAlarm, bnmr.$('.list__li--delete'));
 	}
 };
@@ -279,14 +285,14 @@ bnmr.fns.alarms = function (type) {
 	switch (type) {
 		case 'get':
 			bnmr.vars.alarms = [];
-			
+
 			if (window.localStorage) {
 				if (localStorage.getItem('alarms')) {
 					bnmr.vars.alarms = JSON.parse(localStorage.getItem('alarms'));
 				}
 			}
 		break;
-			
+
 		case 'set':
 			var time, sound, gestures, hour, min, mer, name,
 					newAlarm = {};
@@ -306,18 +312,18 @@ bnmr.fns.alarms = function (type) {
 			name = hour + ':' + min + ' ' + mer;
 
 			newAlarm["name"] = name;
-			
-			bnmr.vars.alarms.push(newAlarm);			
+
+			bnmr.vars.alarms.push(newAlarm);
 			bnmr.fns.closeResetConfig();
-			
+
 			if (window.localStorage) {
 				localStorage.setItem('alarms', JSON.stringify(bnmr.vars.alarms));
 			}
 		break;
-			
+
 		case 'update':
 			var itemToUpdate, name, hour, min, mer;
-			
+
 			for (var i = 0; i < bnmr.vars.alarms.length; i++) {
 				if (bnmr.vars.alarms[i].id == bnmr.vars.selectedAlarm) {
 					itemToUpdate = bnmr.vars.alarms[i];
@@ -339,19 +345,19 @@ bnmr.fns.alarms = function (type) {
 			name = hour + ':' + min + ' ' + mer;
 
 			itemToUpdate["name"] = name;
-			
+
 			document.querySelector('.list__li--alarm[data-alarm-id="' + bnmr.vars.selectedAlarm + '"]').innerHTML = name;
 			document.querySelector('.list__li--alarm[data-alarm-id="' + bnmr.vars.selectedAlarm + '"]').setAttribute('data-alarm-time', itemToUpdate["time"]);
 			document.querySelector('.list__li--alarm[data-alarm-id="' + bnmr.vars.selectedAlarm + '"]').setAttribute('data-alarm-sound', itemToUpdate["sound"]);
 			document.querySelector('.list__li--alarm[data-alarm-id="' + bnmr.vars.selectedAlarm + '"]').setAttribute('data-alarm-gestures', itemToUpdate["gestures"]);
 
 			bnmr.fns.closeResetConfig();
-			
+
 			if (window.localStorage) {
 				localStorage.setItem('alarms', JSON.stringify(bnmr.vars.alarms));
 			}
 		break;
-			
+
 		case 'remove':
 			for (var i = 0; i < bnmr.vars.alarms.length; i++) {
 				if (bnmr.vars.alarms[i].id == bnmr.vars.selectedAlarm) {
@@ -359,26 +365,26 @@ bnmr.fns.alarms = function (type) {
 					bnmr.$('.list__ul').removeChild(document.querySelector('.list__li--alarm[data-alarm-id="' + bnmr.vars.selectedAlarm + '"]'));
 				}
 			}
-			
+
 			if (window.localStorage) {
 				localStorage.setItem('alarms', JSON.stringify(bnmr.vars.alarms));
 			}
 		break;
-			
+
 		case 'clear':
 			var currAlarms = document.querySelectorAll('.list__li--alarm');
 			bnmr.vars.alarms = [];
-			
+
 			for (var i = 0; i < currAlarms.length; i++) {
 				bnmr.$('.list__ul').removeChild(currAlarms[i]);
 			}
-			
+
 			if (window.localStorage) {
 				localStorage.setItem('alarms', '');
 			}
 		break;
 	}
-	
+
 	bnmr.fns.buildAlarmList();
 };
 
@@ -390,7 +396,7 @@ bnmr.fns.stopAlarm = function () {
 bnmr.fns.startAlarm = function () {
 	var randGest = bnmr.vars.arrGestures[(Math.floor(Math.random() * bnmr.vars.arrGestures.length))],
 			overlay = document.createElement('div');
-	
+
 	bnmr.$('#sounds').currentTime = bnmr.vars.objSounds[bnmr.vars.alarmSound].start;
 	bnmr.$('#sounds').play();
 	bnmr.$('#sounds').ontimeupdate = function () {
@@ -398,15 +404,15 @@ bnmr.fns.startAlarm = function () {
 			bnmr.$('#sounds').currentTime = bnmr.vars.objSounds[bnmr.vars.alarmSound].start;
 		}
 	}
-	
+
 	overlay.innerHTML = randGest;
 	overlay.className = 'overlay';
-	
+
 	document.body.appendChild(overlay);
-	
+
 	bnmr.$('.overlay').on(randGest, function () {
 		document.body.removeChild(overlay);
-		
+
 		if (bnmr.vars.numGesturesLeft > 1) {
 			bnmr.vars.numGesturesLeft--;
 			bnmr.fns.startAlarm();
@@ -422,11 +428,11 @@ bnmr.fns.checkForAlarm = function () {
 		now = new Date();
 		nHour = String(now.getHours());
 		nMin = String(now.getMinutes());
-		
+
 		for (var i = 0; i < bnmr.vars.alarms.length; i++) {
 			aHour = bnmr.vars.alarms[i].time.split(':')[0];
 			aMin = bnmr.vars.alarms[i].time.split(':')[1];
-			
+
 			if (nHour == aHour) {
 				if (nMin == aMin) {
 					clearInterval(bnmr.vars.alarmInterval);
@@ -450,14 +456,14 @@ bnmr.fns.eventListeners = function () {
 
 bnmr.fns.init = function () {
 	bnmr.vars.panelState = 'clock';
-	
+
 	bnmr.vars.arrGestures = [
 		'swipeup',
 		'swiperight',
 		'swipedown',
 		'swipeleft'
 	];
-	
+
 	bnmr.vars.objSounds = {
 		"baby": {
 			"start": "0",
@@ -480,11 +486,11 @@ bnmr.fns.init = function () {
 			"end": "39"
 		}
 	}
-	
+
 	bnmr.fns.alarms('get');
 	bnmr.fns.initClockFace();
 	bnmr.fns.eventListeners();
-	
+
 	bnmr.fns.checkForAlarm();
 };
 
