a93bbb1549218b0ab3be3c75c404d3019e252457
Lisa Benmore <lbenmore@gmail.com>
2019-05-14 18:51:33 -0700

    utility testing

--- dev/php/index.php
+++ dev/php/index.php
@@ -3,20 +3,20 @@
 class DB {
 	public static $con;
 	
-	__construct () {
+	function __construct () {
 		$server = "localhost";
 		$username = "lisabe7";
 		$password = "cobain612";
 		$db = "lisabe7_noteboard";
 		
-		$con = new mysqli($server, $username, $password, $db);
+		self::$con = new mysqli($server, $username, $password, $db);
 	}
 }
 
 $db = new DB();
 
 $sql = "SELECT * FROM users";
-if ($stmt = $db::$con->query($sql) {
+if ($stmt = $db::$con->query($sql)) {
 	while ($row = $stmt->fetch_assoc()) {
 		echo $row;
 	}
