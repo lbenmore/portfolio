9aa5a7e9a5a2a27f5b9f0dd7a8d9515a5723dd88
Lisa Benmore <lbenmore@gmail.com>
2019-05-14 18:48:13 -0700

    utility testing

--- dev/php/index.php
+++ dev/php/index.php
@@ -1,3 +1,25 @@
 <?php 
 
+class DB {
+	public static $con;
+	
+	__construct () {
+		$server = "localhost";
+		$username = "lisabe7";
+		$password = "cobain612";
+		$db = "lisabe7_noteboard";
+		
+		$con = new mysqli($server, $username, $password, $db);
+	}
+}
+
+$db = new DB();
+
+$sql = "SELECT * FROM users";
+if ($stmt = $db::$con->query($sql) {
+	while ($row = $stmt->fetch_assoc()) {
+		echo $row;
+	}
+}
+
 ?>
