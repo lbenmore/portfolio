19cdb9076347320bad41cc9058f0e7d0f2e9acaf
Lisa Benmore <lbenmore@gmail.com>
2019-05-14 19:27:45 -0700

    Added email functionality

--- dev/php/index.php
+++ dev/php/index.php
@@ -13,16 +13,60 @@ class DB {
 	}
 }
 
-$db = new DB();
-
-$sql = "SELECT * FROM users";
-if ($stmt = $db::$con->query($sql)) {
-	while ($row = $stmt->fetch_assoc()) {
-		foreach ($row as $key => $value) {
-			echo "$key: $value<br>";
-		}
-		echo "<br>";
+class Output {
+	private static $arr;
+	
+	public static function add ($key, $value) {
+		self::$arr[$key] = $value;
 	}
+	
+	public static function return ($internal) {
+		if ($internal) {
+			return self::$arr;
+		} else {
+			echo json_encode(self::$arr);
+		}
+	}
+}
+
+
+
+function send_mail ($internal, $from, $subject, $body) {
+	$output = new Output;
+	$output->add("status", 0)
+	$output->add("message", "init send_mail");
+	
+	$headers = "From: $from";
+	
+	if (mail("lbenmore@gmail.com", $subject, $body, $headers)) {
+		$output->add("status", 1)
+		$output->add("message", "Email successfully sent");
+	} else {
+		$output->add("message", "Email not sent");
+	}
+	
+	$output->return($internal);		
+}
+
+
+
+$output = new Output;
+$output->add("status", 0);
+
+if (array_key_exists("action", $_POST) {
+	switch ($_POST["action"]) {
+		case "send_email":
+			send_mail(false, $_POST["from"], $_POST["subject"], $_POST["body"]);
+		break;
+		
+		default:
+			$output->add("message", "Action not recognized");
+			$output->return(false);
+		break;
+	}
+} else {
+	$output->add("message", "No action provided");
+	$output->return(false);
 }
 
 ?>
