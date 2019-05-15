ca6bb627c01b18826a62a4ea60b3be059eec81a1
Lisa Benmore <lbenmore@gmail.com>
2019-05-14 19:50:44 -0700

    utility testing

--- dev/php/index.php
+++ dev/php/index.php
@@ -43,6 +43,8 @@ function send_mail ($internal, $from, $subject, $body) {
 		$output->add("status", 1);
 		$output->add("message", "Email successfully sent");
 	} else {
+		ini_set("display_errors", 1);
+		ini_set("error_reporting", E_ALL);
 		$output->add("message", "Email not sent");
 	}
 	
