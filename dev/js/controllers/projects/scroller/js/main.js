639685ecc791fcb8933007b16a1bacebfa20833f
Lisa Benmore <lbenmore@gmail.com>
2022-04-17 11:48:45 -0700

    use target for width and height css variables

--- dev/js/controllers/projects/scroller/js/main.js
+++ dev/js/controllers/projects/scroller/js/main.js
@@ -1,32 +1,36 @@
 import Scroller from './scroller/scroller.js';
 
 ;(function (win, doc) {
-  function updateStyling (cssVars) {
+  function getCssVars (target) {
+    return {
+      vh: win.innerHeight / 100,
+      w: target.getBoundingClientRect().width / 100,
+      h: target.getBoundingClientRect().height / 100
+    };
+  }
+  
+  function updateStyling (target) {
+    const cssVars = getCssVars(target);
     Object.entries(cssVars).forEach(cssVarSet => {
       const [ prop, value ] = cssVarSet;
-      document.body.style.setProperty(`--${prop}`, `${value}px`);
+      doc.body.style.setProperty(`--${prop}`, `${value}px`);
     });
   }
   
-  function eventListeners () {
-    addEventListener('resize', updateStyling);
+  function eventListeners (target) {
+    addEventListener('resize', updateStyling.bind(this, target));
   }
   
   function initFns () {
     fetch('./js/controllers/projects/scroller/scroller_config.json')
       .then(res => res.json())
       .then(json => {
-        const target = document.querySelector(json.target);
-        const cssVars = {
-          vh: win.innerHeight / 100,
-          w: target.getBoundingClientRect().width / 100,
-          h: target.getBoundingClientRect().height / 100
-        };
+        const target = doc.querySelector(json.target);
         
         new Scroller({ ...json, debug: 0 });
         
-        updateStyling(cssVars);
-        eventListeners(cssVars);
+        updateStyling(target);
+        eventListeners(target);
       })
       .catch(err => console.log('ERROR:', err.message));
   }
